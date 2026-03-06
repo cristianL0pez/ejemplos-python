a9a93df17d5d0283d89d2d9167b1ee84b6f5762c
Cristian López <60026545+cristianL0pez@users.noreply.github.com>
2026-03-05 22:11:03 -0300

    clase 23
    Commented out various print statements and input prompts. Added examples of conditionals and loops with explanations.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-
-
-print("hola mundo cruel!!!!")
+#print("hola mundo cruel!!!!")
 
 # VARIABLES
-
 nombre = "cristian"
 edad = 19
 altura = 2.00
@@ -12,7 +9,6 @@ ciudad = "valparaiso"
 
 
 #TIPOS DE DATOS
-
 str #esto es un texto
 int #esto es un entero
 float #esto es un flotante 
@@ -22,8 +18,72 @@ bool #esto es verdadero  o falso
 #input()
 
 
-texto = input()
-print(texto)
+#numero = int(input("escribe un numero : "))
+#resultado = numero + 1
+#print(resultado)
+#
+
+#INPUT
+""" precio = input("escribe un precio : ")# pedir por consola un dato tipo str
+precio = int(precio) #convertir a entero el str con int(precio)
+resultado = precio + 10 #el precio sumado + 10 ya que ahora es numero entero
+print(resultado)# imprimeme el precio en pantalla """
+
+## CONDICIONALES
+
+
+#if condicion:
+#    codigo
+#comparadores
+""" 
+== igual
+!= diferente
+> mayor
+< menor
+>= mayor o igual
+<= menor o igual 
+"""
+
+
+""" edad = 20
+
+if edad >= 18:
+    print("puede entrar a la disco")
+elif edad >= 50:
+    print("demaciado antiguo!!")
+else:
+    print("no puede entrar a la disco")     """
+
+
+#BUCLES
+
+#WHILE
+#while condicion:
+#    codigo
+# while significa mientras pase algo
+contador = 0
+
+while contador < 10:
+    print(contador)
+    contador += 1
+
+""" while True:
+    texto = input("escribe para salir de bucle")
+
+    if texto == "salir":
+        break """
+
+
+#FOR
+
+#for elemento in lista
+#    codigo
+
+lista = ["hola","a","todos"]
+
+for i in lista:
+    print(i)
+
 
 
 
