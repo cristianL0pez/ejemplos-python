fcdc2fb5f16c59269ae9f3ad45e195baeb16a163
Cristian López <60026545+cristianL0pez@users.noreply.github.com>
2026-03-14 00:52:36 +0000

    push

---
+++ funciones.py
@@ -0,0 +1,27 @@
+def saludar():
+    print("")
+
+saludar() #invoke
+
+            #3       8
+def sumar(numero1, numero2):
+    """
+    funcion que sum
+    """
+    resultado = numero1 + numero2
+    return   resultado
+
+ver = sumar(100,378)
+
+print(ver)
+
+len()
+
+
+
+
+    
+
+    
+
+
